168df8f11caa9ba9fccf6fb3c6cc75937ad5e3b7
Marco Gonzalez <magp2606@gmail.com>
2023-05-03 19:59:06 -0500

    fixed mobile and tablet layout

--- src/components/preact/Ticket.jsx
+++ src/components/preact/Ticket.jsx
@@ -1,12 +1,6 @@
-import { useEffect, useRef, useState } from 'preact/hooks'
-import emailjs from '@emailjs/browser'
-import Toast from './Toast'
+import { useEffect } from 'preact/hooks'
 
 export const Ticket = ({ activeSelector }) => {
-  const formRef = useRef(null)
-  const [sendingMessage, setSendingMessage] = useState(false)
-  const [openToast, setOpenToast] = useState(false)
-  const [toastText, setToastText] = useState('')
   useEffect(() => {
     const $ = (selector) => document.querySelector(selector)
 
@@ -39,15 +33,26 @@ export const Ticket = ({ activeSelector }) => {
       <section class='[perspective:1500px] relative' id='wrapper'>
         <article
           id='ticket'
-          class='rounded-lg w-[375px] aspect-[16/9] md:aspect-[18/9] border-4 md:w-[800px] border-opacity-50 relative p-4 overflow-hidden bg-white/50 backdrop-blur-[2px]'
+          class='rounded-lg w-[300px] aspect-[5/8] lg:aspect-[18/9] border-4 lg:w-[800px] border-opacity-50 relative p-4 overflow-hidden bg-white/50 backdrop-blur-[2px] flex flex-col items-start'
           style={{
             border: activeSelector.border
           }}>
+          <a
+            href='mailto:magp260602@gmail.com'
+            className='flex items-center justify-center gap-2 text-md font-semibold text-gray-800 lg:text-lg'>
+            <img src='/mail2.svg' className='w-10 lg:w-16' />
+            magp260602@gmail.com
+          </a>
+          <a
+            href=''
+            className='flex items-center justify-center gap-2 text-md font-semibold text-gray-800 lg:text-lg'>
+            <img src='/instagram.svg' className='w-10 lg:w-16' />
+            always.devv
+          </a>
           <img
             src={activeSelector.icon}
-            class='absolute bottom-0 -right-36 md:top-12 -z-10 md:w-2/3 opacity-20 transition-all'
+            class='absolute bottom-0 -right-36 lg:top-12 -z-10 lg:w-2/3 opacity-20 transition-all'
           />
-          <div class='absolute inset-0 h-full w-full filter blur-xl -z-10'></div>
         </article>
       </section>
     </>
