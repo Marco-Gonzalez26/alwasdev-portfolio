e65435d4cccd1c35b2ad8bca0e9c9c73bd464205
Marco Gonzalez <magp2606@gmail.com>
2023-05-04 10:23:50 -0500

    fixed ticket layout

--- src/components/preact/Ticket.jsx
+++ src/components/preact/Ticket.jsx
@@ -11,21 +11,24 @@ export const Ticket = ({ activeSelector }) => {
 
     const halfWidth = width / 2
     const halfHeight = height / 2
+    try {
+      document.createEvent('TouchEvent')
+    } catch (error) {
+      wrapper.addEventListener('mousemove', (event) => {
+        const { offsetX, offsetY } = event
 
-    wrapper.addEventListener('mousemove', (event) => {
-      const { offsetX, offsetY } = event
+        const rotationX = ((offsetX - halfWidth) / halfWidth) * 5
+        const rotationY = ((offsetY - halfHeight) / halfHeight) * 5
+        ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+        ticket.style.transition = 'all .1s ease'
+      })
 
-      const rotationX = ((offsetX - halfWidth) / halfWidth) * 5
-      const rotationY = ((offsetY - halfHeight) / halfHeight) * 5
-      ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
-      ticket.style.transition = 'all .1s ease'
-    })
+      wrapper.addEventListener('mouseleave', () => {
+        ticket.style.transform = 'rotateX(0deg) rotateY(0deg)'
 
-    wrapper.addEventListener('mouseleave', () => {
-      ticket.style.transform = 'rotateX(0deg) rotateY(0deg)'
-
-      ticket.style.transition = 'all .5s ease'
-    })
+        ticket.style.transition = 'all .5s ease'
+      })
+    }
   })
 
   return (
@@ -33,12 +36,13 @@ export const Ticket = ({ activeSelector }) => {
       <section class='[perspective:1500px] relative' id='wrapper'>
         <article
           id='ticket'
-          class='rounded-lg w-[300px] aspect-[5/8] lg:aspect-[18/9] border-4 lg:w-[800px] border-opacity-50 relative p-4 overflow-hidden bg-white/50 backdrop-blur-[2px] flex flex-col items-start'
+          class='rounded-lg w-[300px] aspect-[7/8] lg:aspect-[18/9] border-4 lg:w-[800px] border-opacity-50 relative p-4 overflow-hidden bg-white/50 backdrop-blur-[2px] flex flex-col items-start'
           style={{
             border: activeSelector.border
           }}>
           <a
             href='mailto:magp260602@gmail.com'
+            target='_blank'
             className='flex items-center justify-center gap-2 text-md font-semibold text-gray-800 lg:text-lg'>
             <img src='/mail2.svg' className='w-10 lg:w-16' />
             magp260602@gmail.com
